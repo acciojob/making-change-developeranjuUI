9eed0c60ae6d0af5a6130f9f36834a83ab1148ea
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-03-14 12:39:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const makeChange = (c) => {
   // your name here
+	const q = Math.floor(c/25);
+	c %= 25;
+	const d = Math.floor(c/10);
+	c %= 10;
+	const n = Math.floor(c/5);
+	c %= 5;
+	const p = c;
+	return {q,d,n,p};
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+const c = prompt("Enter c: ")
 alert(JSON.stringify(makeChange(c)));
